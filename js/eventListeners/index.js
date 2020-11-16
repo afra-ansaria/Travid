05678c97942fd081e9048d462190ec8f2f99c347
Kapaya <6343027+Kapaya@users.noreply.github.com>
2020-11-15 19:42:28 -0500

    Kapaya (#3)
    * Added country select event listener
    
    * Updated event listener to listen on countryDrops
    
    Co-authored-by: Kapaya <kkatongo@mit.edu>

---
+++ js/eventListeners/index.js
@@ -0,0 +1,10 @@
+const countrySelect = document.querySelector('#countryDrops');
+
+countrySelect.addEventListener('change', (event) => {
+    const alpha2Code = event.target.value;
+    COVIDAPI
+        .getPastDataForCountry(alpha2Code)
+        .then(data => {
+            console.log(data);
+        })
+})
