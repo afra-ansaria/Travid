13f5173efca42a4d0864a544a93a08b652f47bb2
Tochi N <tochitemi@gmail.com>
2020-11-25 14:14:59 -0800

    Update index.js

--- js/eventListeners/index.js
+++ js/eventListeners/index.js
@@ -1,5 +1,9 @@
 const EventListners = function(){
     const countrySelect = document.querySelector('#countryDrops');
+    const stock_search_button = document.getElementById("stock_search_button");
+    const stock_search_input = document.getElementById("stock_search");
+
+
     return {
         addListeners: () => {
             window.addEventListener('resize', () => {
@@ -8,6 +12,28 @@ const EventListners = function(){
             countrySelect.addEventListener('change', async () => {
                 await Charts.drawCovidChart();
                 await NewsFeed.populateNewsFeed();
+            })
+            stock_search_button.addEventListener('click', async (event) => {
+                        event.preventDefault();
+                       const stock_symbol = stock_search_input.value;
+                       new TradingView.widget(
+                        {
+                        "autosize": true,
+                        "symbol": stock_symbol,
+                        "interval": "W",
+                        "timezone": "Etc/UTC",
+                        "theme": "light",
+                        "style": "3",
+                        "locale": "en",
+                        "toolbar_bg": "#f1f3f6",
+                        "enable_publishing": false,
+                        "allow_symbol_change": true,
+                        "container_id": "tradingview_cbc98",
+                        "details": true,
+                        "hide_top_toolbar": true
+                       }
+                        );
+                return false;
             })
         }
     }
